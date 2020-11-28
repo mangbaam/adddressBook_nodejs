0072997fb9f34aa4e301f3dc2b56a0553b65ae57
mangbaam <pmb0836@gmail.com>
2020-11-28 13:27:32 +0900

    Show, Edit, Update, Destroy

--- webserver_practice/contact-book/index.js
+++ webserver_practice/contact-book/index.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
+var methodOverried = require('method-override');
 var app = express();
 
 // DB setting
@@ -29,6 +30,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverried('_method'));
 
 // DB schema
 var contactSchema = mongoose.Schema({
@@ -57,6 +59,34 @@ app.get('/contacts/new', function(req, res) {
 // Contacts - create
 app.post('/contacts', function(req, res) {
     Contact.create(req.body, function(err, contact) {
+        if(err) return res.json(err);
+        res.redirect('/contacts');
+    });
+});
+// Contacts - show
+app.get('/contacts/:id', function(req, res) {
+    Contact.findOne({_id:req.params.id}, function(err, contact) {
+        if(err) return res.json(err);
+        res.render('contacts/show', {contact:contact});
+    });
+});
+// Contacts - edit
+app.get('/contacts/:id/edit', function(req, res) {
+    Contact.findOne({_id:req.params.id}, function(err, contact) {
+        if(err) return res.json(err);
+        res.render('contacts/edit', {contact:contact});
+    });
+});
+// Contacts - update
+app.put('/contacts/:id', function(req, res) {
+    Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact) {
+        if(err) res.json(err);
+        res.redirect('/contact/'+req.params.id);
+    });
+});
+// Contacts - destroy
+app.delete('/contacts/:id', function(req, res) {
+    Contact.deleteOne({_id:req.params.id}, function(err) {
         if(err) return res.json(err);
         res.redirect('/contacts');
     });
